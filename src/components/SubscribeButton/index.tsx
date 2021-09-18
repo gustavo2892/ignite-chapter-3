65d683d87e24f21215bfa9fa6736a641570feb1c
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2021-09-18 11:08:55 -0300

    Config Stripe and use SSR Next

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import styles from './styles.module.scss';
 
-export function SubscribeButton() {
+interface SubscribeButonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButonProps) {
   return (
     <button
       type="button"
