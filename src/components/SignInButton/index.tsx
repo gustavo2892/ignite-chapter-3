b2bc5b69f2ce84030bd466b315a590f231f427ce
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2021-09-18 14:00:04 -0300

    SignIn github api with next-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,26 @@
 import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signout, useSession } from 'next-auth/client';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-
-  return isUserLoggedIn ? (
+  const [session] = useSession();
+  
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signout()}
     >
       <FaGithub color="#04d361"/>
-      Gustavo Freitas
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417"/>
       Sign in with Github
