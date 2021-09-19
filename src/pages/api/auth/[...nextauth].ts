4350684da917077684f8850be26ccc063cb5b4bc
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2021-09-18 21:03:49 -0300

    Create user with faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,10 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from 'next-auth';
+import { signIn } from 'next-auth/client';
 import Providers from 'next-auth/providers';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -9,6 +14,25 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     }),
-    // ...add more providers here
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
 })
